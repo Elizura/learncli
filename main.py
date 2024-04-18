153853a7abbe596f50db11bd19063f06506f91f1
Elisur Freyesus <elisurfz@gmail.com>
2024-04-18 23:32:42 +0300

    setup Terminal ui

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 from injector import Injector, Module, singleton
 from Check.checks import Check
-from Questions.core import Question
 from Questions.repo import QuestionsRepo
+from Terminal.repo import TerminalRepo
 from Terminal.terminal import Terminal
 
-
-def main(repo:QuestionsRepo, ter:Terminal, check:Check):
-    question:Question = repo.get_question(level=1, question_number=1)    
-    question_number = 1
-
-    while True:
-        ter.ask_question(question=question)
-        is_correct = check.check_answer(question=question)        
-        if is_correct:            
-            question_number += 1
-            question:Question = repo.get_question(question_number=question_number)
 
 class AppModule(Module):
     def configure(self, binder):
         binder.bind(QuestionsRepo, to=QuestionsRepo, scope=singleton)
-        binder.bind(Terminal, to=Terminal, scope=singleton)
-        binder.bind(Check, to=Check, scope=singleton)
+        binder.bind(TerminalRepo, to=TerminalRepo, scope=singleton)        
+
 
 if __name__ == "__main__":
-    injector = Injector([AppModule()])
-    qrepo = injector.get(QuestionsRepo)
+    injector = Injector([AppModule()])    
     terminal = injector.get(Terminal)
-    check = injector.get(Check)       
-    main(qrepo, terminal, check)
+    terminal.run()
